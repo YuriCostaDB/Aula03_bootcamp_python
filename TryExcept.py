17e9f3087832d9f5bfd8e4f4f21655d2c41c7d6d
Yuri Gomes da Costa <yurigomescosta@outlook.com>
2026-07-27 10:17:54 -0300

    Ajuste main.py e inclusão do exemplo de Try Except

---
+++ TryExcept.py
@@ -0,0 +1,5 @@
+try:
+    gols = int(input("Quantos gols o Brasil fez? "))
+    print(f"O Brasil fez {gols} gols!")
+except Exception as e:
+    print(e)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Esse é o meu primeiro comando!")
+x = 5
+
+print("Esse é o meu segundo comando!")
+x = 6
+
+print("Esse é o meu terceiro comando!")
+
+print(x)
